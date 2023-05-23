831ada5326c7569d705d21541aaf39f94419b470
l9zyr <l9zyr95@163.com>
2023-05-23 12:06:26 +0800

    0523

--- spring-1/src/main/java/edu/lzu/spring/TestUser.java
+++ spring-1/src/main/java/edu/lzu/spring/TestUser.java
@@ -1,31 +1,46 @@
 package edu.lzu.spring;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestUser {
+
+    // 声明手动写日志的对象
+    private Logger logger = LoggerFactory.getLogger(TestUser.class);
+
     @Test
     public void testUserObject() {
-        // 加载spring配置文件
+
+        // 1、加载spring配置文件
         ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
 
-        // 获取创建的对像
+        // 2、获取创建的对像   -id属性值
         User user = (User)context.getBean("user");
         System.out.println("1:" + user);
 
-        // 调用对象的方法
+        System.out.print("2:");
+
+        // 3、调用对象的方法
         user.add();
+
+        // 手动写日志
+        logger.info("######### 执行成功了！！！");
     }
 
 
     // 反射回顾 - 创建对象
     @Test
     public void testUserObject1() throws Exception {
+
+        // 拿到类
         Class aClass = Class.forName("edu.lzu.spring.User");
 
         // User user = (User)aClass.newInstance();
 
+        // 反射创建对象
         User user = (User)aClass.getDeclaredConstructor().newInstance();
 
         System.out.println(user);
